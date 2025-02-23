dbdae0f603f1321d437bf7e28ade99db0db971be
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-02-23 19:19:50 +0000

    chore: initial commit

---
+++ raspberry_pi/src/main.py
@@ -0,0 +1,18 @@
+'''
+App entry point
+Author: Tom Aston
+'''
+import random
+import time
+
+def main() -> None:
+    '''
+    Main function
+    '''
+    while True:
+        mock_cpu_utilisation = random.randint(0, 100)
+        print(f"CPU Utilisation: {mock_cpu_utilisation}%")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
