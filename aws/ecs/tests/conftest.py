9b1142762f773ff17cfc1b617457abeebe2a5d4c
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-03-08 20:42:41 +0000

    tests: cpu metric service unit tests

--- aws/ecs/tests/conftest.py
+++ aws/ecs/tests/conftest.py
@@ -3,20 +3,24 @@ Global fixtures for tests
 Author: Tom Aston
 """
 
+import time
+import uuid
+from typing import Generator
+
 import pytest
 from fastapi.testclient import TestClient
-from src.cpu_metrics.schemas import CpuMetricCreateSchema
+from src.cpu_metrics.schemas import CpuMetricCreateSchema, CpuMetricSchema
 from src.main import app
 
 
 @pytest.fixture
-def test_client() -> TestClient:
+def test_client() -> Generator[TestClient, None, None]:
     """test client fixture
 
     Returns:
         TestClient: test client
     """
-    return TestClient(app)
+    yield TestClient(app)
 
 
 @pytest.fixture
@@ -58,3 +62,57 @@ def test_create_payload() -> list[CpuMetricCreateSchema]:
             version="1.0",
         ),
     ]
+
+
+@pytest.fixture
+def test_cpu_metrics_response_json() -> CpuMetricSchema:
+    """test repspone to get all cpu metrics
+
+    Returns:
+        List[CpuMetricSchema]: dummy json payload
+    """
+    return [
+        CpuMetricSchema(
+            location="Home",
+            cpu_usage=10,
+            unit="percent",
+            loop_count=1,
+            project="test",
+            topic="test",
+            device="test",
+            version="1.0",
+            id=str(uuid.uuid4()),
+            timestamp=int(time.time()),
+        ),
+        CpuMetricSchema(
+            location="Office",
+            cpu_usage=20,
+            unit="percent",
+            loop_count=1,
+            project="test",
+            topic="test",
+            device="test",
+            version="1.0",
+            id=str(uuid.uuid4()),
+            timestamp=int(time.time()),
+        ),
+        CpuMetricSchema(
+            location="Factory",
+            cpu_usage=30,
+            unit="percent",
+            loop_count=1,
+            project="test",
+            topic="test",
+            device="test",
+            version="1.0",
+            id=str(uuid.uuid4()),
+            timestamp=int(time.time()),
+        ),
+    ]
+
+
+def mock_client() -> TestClient:
+    """
+    Test client
+    """
+    return TestClient(app)
--- aws/ecs/tests/cpu_metrics/test_routes.py
+++ aws/ecs/tests/cpu_metrics/test_routes.py
@@ -3,14 +3,14 @@ Unit tests for the routes module
 Author: Tom Aston
 """
 
+from src.config import config_manager
 
-class TestSuiteCpuMetricRoutes:
+API_VERSION = config_manager.VERSION
+
+
+class TestUnitCpuMetricRoutes:
     """
     Test suite for the routes module in CPU Metrics API
     """
 
-    def test_get_all_cpu_metrics(self):
-        """
-        Test get all cpu metrics
-        """
-        assert True
+    pass
